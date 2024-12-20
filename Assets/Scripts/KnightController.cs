f0cad8758d8ad08488fa0706429cdc0459d95e9e
vietlinhg4 <vietlinhg7@gmail.com>
2024-12-20 17:51:56 +0700

    finish jump

--- Assets/Scripts/KnightController.cs
+++ Assets/Scripts/KnightController.cs
@@ -2,21 +2,24 @@ using UnityEngine;
 
 public class KnightController : MonoBehaviour
 {
-    public float timeInvincible = 2.0f;
-    bool isInvincible;
-    float invincibleTimer;
+    //public float timeInvincible = 2.0f;
+    //bool isInvincible;
+    //float invincibleTimer;
     public float speed = 2.5f;
-    public int maxHealth = 5;
-    public int health { get { return currentHealth; } }
-    public int currentHealth;
-    Rigidbody2D rigidbody2d;
+    public float jumpForce = 2.5f;
+    //public int maxHealth = 5;
+    //public int health { get { return currentHealth; } }
+    //public int currentHealth;
+    private Rigidbody2D rigidbody2d;
+    private BoxCollider2D boxCollider;
     float horizontal;
     float vertical;
     Vector2 lookDirection;
     public Animator animator;
-    public GameObject projectilePrefab;
+    //public GameObject projectilePrefab;
     AudioSource audioSource;
     private SpriteRenderer spriteRenderer;
+    [SerializeField] private LayerMask groundLayer;
     //void Launch()
     //{
     //    GameObject projectileObject = Instantiate(projectilePrefab, rigidbody2d.position + Vector2.up * 0.5f,
@@ -27,25 +30,26 @@ public class KnightController : MonoBehaviour
     //    projectile.Launch(lookDirection, 300);
     //    animator.SetTrigger("Launch");
     //}
-    public void ChangeHealth(int amount)
-    {
-        if (amount < 0)
-        {
-            animator.SetTrigger("Hit");
-            if (isInvincible) return;
-            isInvincible = true; invincibleTimer = timeInvincible;
-        }
-        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
-        //UIHealthBar.instance.SetValue(currentHealth / (float)maxHealth);
-    }
+    //public void ChangeHealth(int amount)
+    //{
+    //    if (amount < 0)
+    //    {
+    //        animator.SetTrigger("Hit");
+    //        if (isInvincible) return;
+    //        isInvincible = true; invincibleTimer = timeInvincible;
+    //    }
+    //    currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+    //    //UIHealthBar.instance.SetValue(currentHealth / (float)maxHealth);
+    //}
     // Start is called before the first frame update
     void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
-        currentHealth = maxHealth;
+       /* currentHealth = maxHealth*/;
         audioSource = GetComponent<AudioSource>();
         spriteRenderer = GetComponent<SpriteRenderer>();
+        boxCollider = GetComponent<BoxCollider2D>();
     }
 
     public void PlaySound(AudioClip clip)
@@ -56,17 +60,18 @@ public class KnightController : MonoBehaviour
     void Update()
     {
         horizontal = Input.GetAxis("Horizontal");
-        if (isInvincible)
-        {
-            invincibleTimer -= Time.deltaTime;
-            if (invincibleTimer < 0)
-                isInvincible = false;
-        }
-        Vector2 move = new Vector2(horizontal, 0.0f);
-        if (!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
-        {
-            spriteRenderer.flipX = move.x < 0.0f;
-        }
+        //if (isInvincible)
+        //{
+        //    invincibleTimer -= Time.deltaTime;
+        //    if (invincibleTimer < 0)
+        //        isInvincible = false;
+        //}
+        if (horizontal > 0.01f)
+            transform.localScale = new Vector3(-1, 1, 1);
+        else if (horizontal < -0.01f)
+            transform.localScale = Vector3.one;
+        if (Input.GetKey(KeyCode.Space))
+            Jump();
         //if (Input.GetKeyDown(KeyCode.Space))
         //{
         //    Launch();
@@ -87,13 +92,28 @@ public class KnightController : MonoBehaviour
         //}
         //animator.SetFloat("Look X", lookDirection.x);
         //animator.SetFloat("Look Y", lookDirection.y);
-        animator.SetFloat("Speed", move.magnitude);
+        animator.SetBool("run", horizontal != 0);
+        animator.SetFloat("yVelocity", rigidbody2d.linearVelocity.y);
+        animator.SetBool("jump", !isGrounded());
     }
     private void FixedUpdate()
     {
-        Vector2 position = transform.position;
-        position.x = position.x + speed * horizontal * Time.deltaTime;
-        position.y = position.y + speed * vertical * Time.deltaTime;
-        rigidbody2d.MovePosition(position);
+        rigidbody2d.linearVelocity = new Vector2(Input.GetAxis("Horizontal") * speed, rigidbody2d.linearVelocity.y);
+    }
+    private void Jump()
+    {
+        if (isGrounded())
+        {
+            rigidbody2d.linearVelocity = Vector2.up * jumpForce;
+            animator.SetTrigger("jump");
+        }
+        
+    }
+    private bool isGrounded()
+    {
+        // Adjust the size to be slightly smaller in width, ensuring it only checks below the character
+        Vector2 boxSize = new Vector2(boxCollider.bounds.size.x, boxCollider.bounds.size.y);
+        RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider.bounds.center, boxSize, 0, Vector2.down, 0.1f, groundLayer);
+        return raycastHit.collider != null;
     }
 }

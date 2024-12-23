989fcca2929771327b334620d47236b8a376c009
lhda <106147769+lhda@users.noreply.github.com>
2024-12-24 04:13:12 +0700

    Added patroling logic

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -2,7 +2,6 @@ using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 using System;
-using Unity.VisualScripting.ReorderableList;
 
 public enum EnemyState
 {
@@ -16,14 +15,23 @@ public class EnemyController : MonoBehaviour
 {
     #region Declare Variables
 
+    [SerializeField] private float attackCooldown;
+    [SerializeField] private float range;
+    [SerializeField] private int damage;
+
+    [SerializeField] private Health health;
+    [SerializeField] private float colliderDistance;
+    [SerializeField] private LayerMask groundLayer;
+
     private AudioSource audioSource;
     private Rigidbody2D rigidbody2d;
     private BoxCollider2D boxCollider;
     private SpriteRenderer spriteRenderer;
-    private float hurtDuration = 1;
 
-    [SerializeField] private LayerMask groundLayer;
-    [SerializeField] private Health health;
+    private Health playerHealth;
+    private EnemyPatrol enemyPatrol;
+    private float cooldownTimer = Mathf.Infinity;
+
     private EnemyState enemyState = EnemyState.Idle;
     public EnemyState EnemyState
     {
@@ -47,11 +55,8 @@ public class EnemyController : MonoBehaviour
     }
 
     public Animator animator;
-    public float range = 1f;
-    public float jumpForce = 2.5f;
 
     private Vector2 rootPosition = new Vector2();
-    private bool iframe = false;
 
     #endregion Declare Variables
 
@@ -67,7 +72,6 @@ public class EnemyController : MonoBehaviour
         boxCollider = GetComponent<BoxCollider2D>();
 
         rootPosition = gameObject.transform.position;
-        StartCoroutine(Moving());
     }
 
     private void OnEnable()
@@ -75,13 +79,27 @@ public class EnemyController : MonoBehaviour
         enemyState = EnemyState.Moving;
     }
 
-    //private void Update() // Change to coroutine to reduce update call
-    //{
-    //}
+    private void Update()
+    {
+        cooldownTimer += Time.deltaTime;
+
+        //Attack only when player in sight?
+        if (PlayerDetected())
+        {
+            if (cooldownTimer >= attackCooldown)
+            {
+                cooldownTimer = 0;
+                animator.SetTrigger("meleeAttack");
+            }
+        }
+
+        if (enemyPatrol != null)
+            enemyPatrol.enabled = !PlayerDetected();
+    }
 
     private void OnCollisionEnter2D(UnityEngine.Collision2D collision)
     {
-        if (collision.gameObject.name.Contains("AttackArea") && !iframe)
+        if (collision.gameObject.name.Contains("AttackArea"))
         {
             Debug.Log(collision.gameObject.transform);
             rigidbody2d.AddForce(new Vector2(MathF.Sign(gameObject.transform.position.x - collision.gameObject.transform.position.x), 1) * 2.5f, ForceMode2D.Impulse);
@@ -100,37 +118,10 @@ public class EnemyController : MonoBehaviour
 
     #region Enemy Behaviours
 
-    private IEnumerator Moving()
-    {
-        animator.SetTrigger("Move");
-        rootPosition = gameObject.transform.position;
-        while (enemyState == EnemyState.Moving)
-        {
-            Vector2 position = rigidbody2d.position;
-            position.x = rootPosition.x + range * MathF.Sin(Time.fixedTime);
-            rigidbody2d.MovePosition(position);
-            yield return new WaitForSeconds(0.1f);
-        }
-    }
-
     private IEnumerator Hurt()
     {
-        iframe = true;
-        //health.Damage(1);
-        Debug.Log("Hurt");
-
-        if (false) //Health = 0
-        {
-            yield return Death();
-        }
-        else
-        {
-            animator.SetTrigger("Hurt");
-            yield return new WaitForSeconds(animator.GetCurrentAnimatorStateInfo(0).length);
-            StartCoroutine(Moving());
-        }
-
-        iframe = false;
+        animator.SetTrigger("Hurt");
+        yield return new WaitForSeconds(animator.GetCurrentAnimatorStateInfo(0).length);
     }
 
     private IEnumerator Death()
@@ -138,6 +129,34 @@ public class EnemyController : MonoBehaviour
         animator.SetTrigger("Death");
         yield return new WaitForSeconds(animator.GetCurrentAnimatorStateInfo(0).length);
         DestroyImmediate(gameObject);
+    }
+
+    private bool PlayerDetected()
+    {
+        RaycastHit2D hit =
+            Physics2D.BoxCast(boxCollider.bounds.center + transform.right * range * transform.localScale.x * colliderDistance,
+            new Vector3(boxCollider.bounds.size.x * range, boxCollider.bounds.size.y, boxCollider.bounds.size.z),
+            0, Vector2.left, 0, groundLayer);
+
+        if (hit.collider != null)
+            playerHealth = hit.transform.GetComponent<Health>();
+
+        return hit.collider != null;
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        if (boxCollider != null)
+        {
+            Gizmos.DrawWireCube(boxCollider.bounds.center + transform.right * range * transform.localScale.x * colliderDistance,
+                new Vector3(boxCollider.bounds.size.x * range, boxCollider.bounds.size.y, boxCollider.bounds.size.z));
+        }
+    }
+
+    private void DamagePlayer()
+    {
+        if (PlayerDetected())
+            playerHealth.Damage(damage);
     }
 
     #endregion Enemy Behaviours
